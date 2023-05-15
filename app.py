99f679f677a39b603b83aecbc8a280c4c6626336
edizbak_its <valla_jo@hotmail.com>
2023-05-15 12:20:12 +0200

    1st run

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
-from flask import flask
+from flask import Flask, jsonify
+import hashlib
+import time
+import requests
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+PUBLIC_KEY = app.config['MARVEL_PUBLIC_KEY']
+PRIVATE_KEY = app.config['MARVEL_PRIVATE_KEY']
+BASE_URL = 'http://gateway.marvel.com/v1/public/'
+
+
+def generate_hash(ts, private_key, public_key):
+    """Génération de hash pour l'API Marvel"""
+    m = hashlib.md5()
+    m.update(f"{ts}{private_key}{public_key}".encode('utf-8'))
+    return m.hexdigest()
+
+
+@app.route('/characters')
+def get_character():
+    ts = str(time.time())
+    hash = generate_hash(ts, PRIVATE_KEY, PUBLIC_KEY)
+    params = {
+        'apikey': PUBLIC_KEY,
+        'ts': ts,
+        'hash': hash,
+        'limit': 100
+    }
+    response = requests.get(f"{BASE_URL}characters", params=params)
+    return jsonify(response.json())
+
+
+app.run(debug=True)
